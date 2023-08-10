9714459a135355cd5dc4a005ee9d2f28788a6067
fidelisbae <fidelisbae@gmail.com>
2023-08-10 15:18:59 +0900

    초기 커밋.

---
+++ 12865/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _12865
+{
+    public class Item
+    {
+        public int Value
+        {
+            get; private set;
+        }
+
+        public int Weight { get; private set; }
+
+        public Item(int value, int weight)
+        {
+            Value = value;
+            Weight = weight;
+        }
+    }
+
+    internal class Program
+
+    {
+        private static void GetInput(out int totalNumber, out int maxWeight, out Item[] items)
+        {
+            string firstLine = Console.ReadLine();
+            totalNumber = int.Parse(firstLine.Split(" ")[0]);
+            maxWeight = int.Parse(firstLine.Split(" ")[1]);
+
+            items = new Item[totalNumber];
+
+            for (int i = 0; i < totalNumber; i++)
+            {
+                string input = Console.ReadLine();
+                items[i] = new Item(int.Parse(input.Split(" ")[0]), int.Parse(input.Split(" ")[1]));
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            GetInput(out int totalNumber, out int maxWeight, out Item[] items);
+
+            int totalValue = 0;
+
+            foreach (Item item in items)
+            {
+            }
+        }
+    }
+}
---
+++ Backjoon/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1966
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int testCase = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testCase; i++)
+            {
+                string firstInput = Console.ReadLine();
+                string secondInput = Console.ReadLine();
+
+                int numbers = int.Parse(firstInput.Split(" ")[0]);
+                int target = int.Parse(firstInput.Split(' ')[1]);
+
+                var list = new LinkedList<int[]>();
+                int count = 0;
+
+                for (int j = 0; j < numbers; j++)
+                {
+                    if (j == target)
+                    {
+                        list.AddLast(new int[] { int.Parse(secondInput.Split(" ")[j]), 1 });
+                    }
+                    else
+                    {
+                        list.AddLast(new int[] { int.Parse(secondInput.Split(" ")[j]), 0 });
+                    }
+                }
+
+                while (list.Count > 0)
+                {
+                    var temp = list.First;
+                    bool isTarget = false;
+                    bool back = false;
+
+                    if (temp.Value[1] == 1)
+                    {
+                        isTarget = true;
+                    }
+
+                    var current = temp.Next;
+
+                    while (current != null)
+                    {
+                        if (temp.Value[0] < current.Value[0])
+                        {
+                            back = true;
+                            break;
+                        }
+                        current = current.Next;
+                    }
+
+                    if (back)
+                    {
+                        list.RemoveFirst();
+                        list.AddLast(temp);
+                    }
+                    else
+                    {
+                        if (isTarget)
+                        {
+                            Console.WriteLine(count + 1);
+                            list.RemoveFirst();
+                            break;
+                        }
+                        else
+                        {
+                            list.RemoveFirst();
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
