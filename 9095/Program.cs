aa2b74fe50b49f7044e64a16356365d5b4fbd04b
fidelisbae <fidelisbae@gmail.com>
2023-08-23 15:14:24 +0900

    9095 1,2,3 더하기 재귀

--- 9095/Program.cs
+++ 9095/Program.cs
@@ -1,9 +1,28 @@
-﻿using System.Text;
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Text;
 
 namespace _9095
 {
     internal class Program
     {
+        private static int[] dp = new int[11];
+
+        private static int Recursion(int n)
+        {
+            if (n > 10)
+            {
+                return 0;
+            }
+
+            dp[n] = dp[n] + 1;
+
+            Recursion(n + 1);
+            Recursion(n + 2);
+            Recursion(n + 3);
+
+            return 0;
+        }
+
         private static void Main(string[] args)
         {
             var sr = new StreamReader(Console.OpenStandardInput());
@@ -11,15 +30,16 @@ namespace _9095
 
             int N = int.Parse(sr.ReadLine()!);
 
+            Recursion(0);
+
             for (int i = 0; i < N; i++)
             {
                 int number = int.Parse(sr.ReadLine()!);
-                var dp = new int[number + 1];
-                dp[0] = 0;
-                dp[1] = 1;
-                dp[2] = 2;
-                dp[3] = 4;
+
+                sb.AppendLine(dp[number].ToString());
             }
+
+            Console.WriteLine(sb.ToString());
         }
     }
 }
