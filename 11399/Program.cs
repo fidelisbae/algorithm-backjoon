e2f0a38fa7006631037dac3222412302862135d9
fidelisbae <fidelisbae@gmail.com>
2023-08-23 15:32:18 +0900

    11399 ATM 정렬

---
+++ 11399/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _11399
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()!);
+            string[] input = Console.ReadLine()!.Split(" ");
+            var times = new (int index, int time)[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                times[i] = (i + 1, int.Parse(input[i]));
+            }
+
+            Array.Sort(times, (x, y) => x.time.CompareTo(y.time));
+
+            int answer = 0;
+
+            for (int i = 0; i < times.Length; i++)
+            {
+                answer = answer + times[i].time * (times.Length - i);
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
