fc747e9985fed61137009428b5e3a9f43e5a285c
fidelisbae <fidelisbae@gmail.com>
2023-08-24 10:00:47 +0900

    11723 집합 비트마스킹 구현

--- 11723/Program.cs
+++ 11723/Program.cs
@@ -1,55 +1,55 @@
 ﻿using System.Diagnostics.Tracing;
+using System.Text;
 
 namespace _11723
 {
     public class BitSet
     {
-        private long set;
+        private int set;
+        public StringBuilder sb;
 
         public BitSet()
         {
             set = 0;
+            sb = new StringBuilder();
         }
 
-        public void Add(long x)
+        public void Add(int x)
         {
-            long mask = x - 1;
-            mask = 1L << mask;
-            set |= 1L << x - 1L;
+            int mask = 1 << x - 1;
+            set |= mask;
         }
 
-        public void Remove(long x)
+        public void Remove(int x)
         {
-            long mask = 1L << x - 1L;
+            int mask = 1 << x - 1;
             mask = ~mask;
             set &= mask;
         }
 
-        public void Check(long x)
+        public void Check(int x)
         {
-            long mask = 1 << x - 1;
-            long check = set | mask;
+            int mask = 1 << x - 1;
 
-            if (set == check)
+            if ((set & mask) == mask)
             {
-                Console.WriteLine(1);
+                sb.AppendLine("1");
             }
             else
             {
-                Console.WriteLine(0);
+                sb.AppendLine("0");
             }
         }
 
-        public void Toggle(long x)
+        public void Toggle(int x)
         {
-            long mask = 1 << x - 1;
+            int mask = 1 << x - 1;
             set ^= mask;
         }
 
         public void All()
         {
-            set = 0;
-            set = ~set;
+            set = 0b11111111111111111111;
         }
 
         public void Empty()
@@ -62,19 +62,19 @@ namespace _11723
     {
         private static void Main(string[] args)
         {
-            long M = long.Parse(Console.ReadLine()!);
-
+            var sr = new StreamReader(Console.OpenStandardInput());
+            int M = int.Parse(sr.ReadLine()!);
             var set = new BitSet();
 
             for (int i = 0; i < M; i++)
             {
-                string[] input = Console.ReadLine()!.Split(" ");
+                string[] input = sr.ReadLine()!.Split(" ");
                 string word = input[0];
-                long value = 0;
+                int value = 0;
 
                 if (input.Length > 1)
                 {
-                    value = long.Parse(input[1]);
+                    value = int.Parse(input[1]);
                 }
 
                 switch (word)
@@ -107,6 +107,7 @@ namespace _11723
                         break;
                 }
             }
+            Console.WriteLine(set.sb.ToString());
         }
     }
 }
