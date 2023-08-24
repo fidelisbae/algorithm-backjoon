6a470cb80ddbaebad24934cc8f0358a4f033657d
fidelisbae <fidelisbae@gmail.com>
2023-08-24 15:28:53 +0900

    11726 2*n타일링 dp,오버플로우 방지

---
+++ 11726/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _11726
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+            var dp = new int[n + 1];
+            dp[0] = 1;
+            dp[1] = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+            }
+
+            Console.WriteLine(dp[n]);
+        }
+    }
+}
