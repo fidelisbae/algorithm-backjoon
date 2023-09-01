62dbd27e3e5e7e044e808767a421ea82608a9142
fidelisbae <fidelisbae@gmail.com>
2023-09-01 10:34:32 +0900

    1208 부분수열의합2 이진탐색, meet in middle

--- 1208/Program.cs
+++ 1208/Program.cs
@@ -2,80 +2,121 @@
 {
     internal class Program
     {
-        private static int count = 0;
-        private static int N;
-        private static int S;
-        private static int[] numbers;
-        private static int[] halfArray_1;
-        private static int[] halfArray_2;
-        private static List<int> list1;
-        private static List<int> list2;
-
-        private static int Recursion1(int index, int sum)
-        {
-            if (index == halfArray_1.Length)
-            {
-                return 0;
-            }
-
-            list1.Add(sum);
-            Recursion1(index + 1, sum + numbers[index]);
-            Recursion1(index + 1, sum);
-
-            return 0;
-        }
-
-        private static int Recursion2(int index, int sum)
-        {
-            if (index == halfArray_2.Length)
-            {
-                return 0;
-            }
-
-            list2.Add(sum);
-            Recursion2(index + 1, sum + numbers[index]);
-            Recursion2(index + 1, sum);
-
-            return 0;
-        }
+        private static long count = 0;
+        private static long N;
+        private static long S;
+        private static long[] numbers;
+        private static List<long> list1;
+        private static List<long> list2;
 
         private static void GetInput()
         {
             var sr = new StreamReader(Console.OpenStandardInput());
             string[] firstInput = sr.ReadLine()!.Split(" ");
             string[] secondInput = sr.ReadLine()!.Split(" ");
-            N = int.Parse(firstInput[0]);
-            S = int.Parse(firstInput[1]);
-            numbers = new int[N];
-            int mid = N / 2;
-            halfArray_1 = new int[mid];
-            halfArray_2 = new int[numbers.Length - mid];
-            Array.Copy(numbers, 0, halfArray_1, 0, mid);
-            Array.Copy(numbers, mid, halfArray_2, 0, numbers.Length - mid);
-            list1 = new List<int>();
-            list2 = new List<int>();
+            N = long.Parse(firstInput[0]);
+            S = long.Parse(firstInput[1]);
+            numbers = new long[N];
+            list1 = new List<long>();
+            list2 = new List<long>();
 
-            for (int i = 0; i < N; i++)
+            for (long i = 0; i < N; i++)
             {
-                numbers[i] = int.Parse(secondInput[i]);
+                numbers[i] = long.Parse(secondInput[i]);
             }
+        }
+
+        private static long Recursion1(long index, long sum)
+        {
+            if (index == N / 2)
+            {
+                list1.Add(sum);
+                return 0;
+            }
+
+            Recursion1(index + 1, sum + numbers[index]);
+            Recursion1(index + 1, sum);
+
+            return 0;
+        }
+
+        private static long Recursion2(long index, long sum)
+        {
+            if (index == N)
+            {
+                list2.Add(sum);
+                return 0;
+            }
+
+            Recursion2(index + 1, sum + numbers[index]);
+            Recursion2(index + 1, sum);
+
+            return 0;
+        }
+
+        private static long FindLow(long target)
+        {
+            long left = 0;
+            long right = list2.Count - 1;
+
+            while (left <= right)
+            {
+                long mid = (left + right) / 2;
+
+                if (list2[(int)mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return left;
+        }
+
+        private static long FindHigh(long target)
+        {
+            long left = 0;
+            long right = list2.Count - 1;
+
+            while (left <= right)
+            {
+                long mid = (left + right) / 2;
+
+                if (list2[(int)mid] <= target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return left;
         }
 
         private static void Main(string[] args)
         {
             GetInput();
-            Recursion1(0, halfArray_1[0]);
-            Recursion2(0, halfArray_2[0]);
+            Recursion1(0, 0);
+            Recursion2(N / 2, 0);
+            list2.Sort();
 
-            foreach (var sum1 in list1)
+            for (int i = 0; i < list1.Count; i++)
             {
-                foreach (var sum2 in list2)
-                {
-                    if (sum1 + sum2 == S)
-                    {
-                        count++;
-                    }
-                }
+                long target = S - list1[i];
+                long left = FindLow(target);
+                long right = FindHigh(target);
+
+                count = count + (right - left);
+            }
+
+            if (S == 0)
+            {
+                count--;
             }
 
             Console.WriteLine(count);
