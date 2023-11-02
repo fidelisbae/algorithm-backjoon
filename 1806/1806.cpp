35ae81e1ef41f5017b766227789a4d5e57aa7c8d
fidelisbae <fidelisbae@gmail.com>
2023-11-02 12:41:09 +0900

    1806 부분합 투포인터

---
+++ 1806/1806.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+int main()
+{
+	int N;
+	int S;
+
+	std::cin >> N;
+	std::cin >> S;
+
+	std::vector<int> numbers(N);
+
+	for (int i = 0; i < N; i++) {
+		std::cin >> numbers[i];
+	}
+
+	int sum = 0;
+	int start = 0;
+	int end = 0;
+	int min = INT_MAX;
+
+	while (true)
+	{
+		if (sum >= S) {
+			min = std::min(min, end - start);
+			sum -= numbers[start++];
+		}
+		else if (end < N) {
+			sum += numbers[end++];
+		}
+		else {
+			break;
+		}
+	}
+
+	if (min == INT_MAX) {
+		std::cout << 0;
+	}
+	else {
+		std::cout << min;
+	}
+
+	return 0;
+}
